01dfe04675f5d3ea3dc13556edbd8551bcae44b7
KhangItWebDev <khang01279567920@gmail.com>
2023-02-09 11:45:08 +0700

    push codecode

--- clientAxios/bookingClientAxios.js
+++ clientAxios/bookingClientAxios.js
@@ -7,8 +7,9 @@ const bookingClientAxios = axios.create({
   baseURL: "https://betatgh.fostech.vn",
   timeout: 20000,
   headers: {
-    "Access-Control-Allow-Methods": "GET, POST",
-    "Access-Control-Allow-Origin": "*",
+    // "Access-Control-Allow-Methods": "GET, POST",
+    // "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": false,
   },
   paramsSerializer: (params) => queryString.stringify(params),
 });

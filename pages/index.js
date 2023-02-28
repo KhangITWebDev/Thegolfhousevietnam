dd6d0a084aea152426711bbb08fa0438442ac1b2
KhangItWebDev <khang01279567920@gmail.com>
2023-02-28 16:58:49 +0700

    add

--- pages/index.js
+++ pages/index.js
@@ -6,20 +6,8 @@ import Seo from "../components/Seo/seo";
 
 export default function Home() {
   return (
-    <>
-      <Seo
-        data={{
-          title: "WordPress Themes & Website Templates from ThemeForest",
-          description:
-            "Step by step tutorials to build a full CRUD website using NextJS for beginners",
-          url: "https://saas-ecommerce.vercel.app/",
-          thumbnailUrl:
-            "https://cdn.create.vista.com/downloads/348b83d7-7b70-4496-ba3f-b052b3454117_1024.jpeg",
-        }}
-      />
-      <MainLayout>
-        <HomePage />
-      </MainLayout>
-    </>
+    <MainLayout>
+      <HomePage />
+    </MainLayout>
   );
 }
